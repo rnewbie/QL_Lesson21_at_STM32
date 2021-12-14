414ff0a2942affdeca48ff6d2811de9d0cdc3e63
Alexey Pavlov <rnewbie@hotmail.com>
2021-12-14 13:58:07 +0300

    Lesson22, end

--- Src/main.c
+++ Src/main.c
@@ -19,6 +19,9 @@
 #include <stdint.h>
 #include "bsp.h"
 
+uint32_t stack_blinky1[40];
+uint32_t *sp_blinky1 = &stack_blinky1[40];
+
 void main_blinky1(void) {
 	while (1) {
 		BSP_ledGreenOn();
@@ -27,6 +30,9 @@ void main_blinky1(void) {
 		BSP_delay(BSP_TICKS_PER_SEC * 3U / 4U);
 	}
 }
+
+uint32_t stack_blinky2[40];
+uint32_t *sp_blinky2 = &stack_blinky2[40];
 
 void main_blinky2(void) {
 	while (1) {
@@ -38,12 +44,46 @@ void main_blinky2(void) {
 }
 
 int main(void) {
-	uint32_t volatile run = 0U;
 
 	BSP_init();
-	if (run) {
-		main_blinky1();
-	} else {
-		main_blinky2();
+	/* Stack for blinky1 */
+	*(--sp_blinky1) = (1<<24U); /* xPSR */
+	*(--sp_blinky1) = (uint32_t)&main_blinky1; /* PC */
+	*(--sp_blinky1) = 0x0000000EU; /* LR */
+	*(--sp_blinky1) = 0x0000000CU; /* R12 */
+	*(--sp_blinky1) = 0x00000003U; /* R3 */
+	*(--sp_blinky1) = 0x00000002U; /* R2 */
+	*(--sp_blinky1) = 0x00000001U; /* R1 */
+	*(--sp_blinky1) = 0x00000000U; /* R0 */
+
+	*(--sp_blinky1) = 0x0000000BU; /* R11 */
+	*(--sp_blinky1) = 0x0000000AU; /* R10 */
+	*(--sp_blinky1) = 0x00000009U; /* R9 */
+	*(--sp_blinky1) = 0x00000008U; /* R8 */
+	*(--sp_blinky1) = 0x00000007U; /* R7 */
+	*(--sp_blinky1) = 0x00000006U; /* R6 */
+	*(--sp_blinky1) = 0x00000005U; /* R5 */
+	*(--sp_blinky1) = 0x00000004U; /* R4 */
+
+	/* Stack for blinky2 */
+	*(--sp_blinky2) = (1<<24U); /* xPSR */
+	*(--sp_blinky2) = (uint32_t)&main_blinky2; /* PC */
+	*(--sp_blinky2) = 0x0000000EU; /* LR */
+	*(--sp_blinky2) = 0x0000000CU; /* R12 */
+	*(--sp_blinky2) = 0x00000003U; /* R3 */
+	*(--sp_blinky2) = 0x00000002U; /* R2 */
+	*(--sp_blinky2) = 0x00000001U; /* R1 */
+	*(--sp_blinky2) = 0x00000000U; /* R0 */
+
+	*(--sp_blinky2) = 0x0000000BU; /* R11 */
+	*(--sp_blinky2) = 0x0000000AU; /* R10 */
+	*(--sp_blinky2) = 0x00000009U; /* R9 */
+	*(--sp_blinky2) = 0x00000008U; /* R8 */
+	*(--sp_blinky2) = 0x00000007U; /* R7 */
+	*(--sp_blinky2) = 0x00000006U; /* R6 */
+	*(--sp_blinky2) = 0x00000005U; /* R5 */
+	*(--sp_blinky2) = 0x00000004U; /* R4 */
+	while(1) {
+//		__NOP();
 	}
 }
